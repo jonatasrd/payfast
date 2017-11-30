9e31ed3b7c50b30318a7dddbcd3eac681d7d5c30
Jonatas Ramos <jonatasrd@gmail.com>
2017-11-30 21:56:17 -0200

    Setup inicial do projetos

---
+++ controllers/pagamentos.js
@@ -0,0 +1,8 @@
+
+module.exports = function(app){
+    app.get('/pagamentos', function(req, res){
+      console.log('Recebida requisicao de teste na porta 3000.')
+      res.send('OK.');
+    });
+  }
+  
